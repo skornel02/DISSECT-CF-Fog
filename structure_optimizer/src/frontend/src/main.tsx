6e65c495310aa3cee02ed8ace16cf5209d9eab6f
Stefán Kornél <stefankornel02@gmail.com>
2024-11-20 14:16:09 +0100

    404 fallback

--- structure_optimizer/src/frontend/src/main.tsx
+++ structure_optimizer/src/frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {createHashRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {routes} from "@/routes.tsx";
 import './index.css'
 
-const router = createHashRouter(routes);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
